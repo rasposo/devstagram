0c2ca34f177b8f72e36a0a6511056c33e6bdc69f
rasposo <rasposo@gmail.com>
2023-06-25 18:15:52 +0200

    hasta validacion

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+   
+    public function index() 
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request);
+        //dd($request->get('username'));
+
+        //Validación
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required'
+        ]);
+
+
+    }
+}
